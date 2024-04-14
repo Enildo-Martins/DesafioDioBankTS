f52fd2fb2f4276279bd06a2158ab48336a947496
Enildo Martins de Oliveira Filho <127439689+Enildo-Martins@users.noreply.github.com>
2024-04-14 15:40:37 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,7 @@
+import { DioAccount } from "./class/DioAccount"
+import { PeopleAccount } from "./class/PeopleAccount"
+import { CompanyAccount } from "./class/CompanyAccount"
+
+const account1: PeopleAccount = new PeopleAccount("Enildo", 5165, 115)
+const peopleCompany: CompanyAccount = new CompanyAccount("Aliare", 2544)
+
