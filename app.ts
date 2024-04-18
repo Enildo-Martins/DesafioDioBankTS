80cb784ecf54895dc825b560da1f67b9fcf09f39
Enildo Martins de Oliveira Filho <127439689+Enildo-Martins@users.noreply.github.com>
2024-04-18 13:39:09 -0300

    Update app.ts
    Atualizando a classe "app" onde nela é realizado todos os metodos possiveis do DioBank.

--- app.ts
+++ app.ts
@@ -1,7 +1,34 @@
-import { DioAccount } from "./class/DioAccount"
-import { PeopleAccount } from "./class/PeopleAccount"
-import { CompanyAccount } from "./class/CompanyAccount"
+import { DioAccount } from "./class/DioAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
+import { CompanyAccount } from "./class/CompanyAccount";
+import { SimpleAccount } from "./class/SimpleAccount";
 
-const account1: PeopleAccount = new PeopleAccount("Enildo", 5165, 115)
-const peopleCompany: CompanyAccount = new CompanyAccount("Aliare", 2544)
+const accountDio: DioAccount = new DioAccount("Enildo Filho", 665496);
+const accountCompany: CompanyAccount = new CompanyAccount("Enildo", 23654);
+const accountPeople: PeopleAccount = new PeopleAccount("Julia", 12699, 12345);
+const accountSimple: SimpleAccount = new SimpleAccount("Vânia", 21565);
 
+console.log(accountDio);
+console.log("Saldo:", accountDio.getBalance());
+accountDio.deposit(100);
+accountDio.whithdraw(50);
+console.log("");
+
+console.log(accountCompany);
+console.log("Saldo: ", accountCompany.getBalance());
+accountCompany.getLoan(1000);
+console.log("Saldo: ", accountCompany.getBalance());
+accountCompany.whithdraw(500);
+console.log("");
+
+console.log(accountPeople);
+console.log("Saldo: ", accountPeople.getBalance());
+accountPeople.deposit(500);
+accountPeople.whithdraw(100);
+console.log("");
+
+console.log(accountSimple);
+console.log("Saldo: ", accountSimple.getBalance());
+accountSimple.depositTenMore(200);
+accountSimple.whithdraw(100);
+console.log("");
